dbf256d2462d2503674804f2a52ba1790b2f67af
Alexander Lim <me@alexanderlim.com>
2016-09-27 02:39:16 +0900

    Remove reference to font CSS

--- gendefault.sh
+++ gendefault.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$PWD/generate_resume.py --outfile resume.html --css screen.css --font-css fonts.css --print-css print.css --inline-css -- template.html data.json
+$PWD/generate_resume.py --outfile resume.html --css screen.css --print-css print.css --inline-css -- template.html data.json
